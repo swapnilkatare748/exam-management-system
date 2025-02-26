b1a8d41a88e9cf6818a295ad9e459953c514ad4b
swapnil katare <swapnilkatare748@gmail.com>
2025-02-26 16:53:54 +0530

    added the readme

--- Website/src/Components/SignUppageComponents/SignUp/SignUp.jsx
+++ Website/src/Components/SignUppageComponents/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import styles from "./SignUP.module.css"; // Import modular CSS
+import styles from "./SignUp.module.css"; // Import modular CSS
 import { FiCodesandbox } from "react-icons/fi";
 import { COMPANY_NAME } from "../../../Data.jsx";
 import { useNavigate } from "react-router-dom";
