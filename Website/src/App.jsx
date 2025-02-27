672bb4d03350302fe7ad27b0d6199af74d28c8e5
swapnil katare <swapnilkatare748@gmail.com>
2025-02-27 11:56:58 +0530

    added the ExamPage page  error resolve

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
        <Routes>
           <Route path='/' element={<LoginPage/>}/>
           <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/signUp' element={<SignUPpage/>}/>
+          {/* <Route path='/signUp' element={<SignUPpage/>}/> */}
           
                  
           {/* Protected routes */}
